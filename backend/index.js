e87df2ec9700872195e5c96d278015e45af64e96
nishantsingh6 <nishantsingh2n@gmail.com>
2026-04-10 16:38:37 +0530

    update cors url

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ const port = process.env.PORT || 5000;;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://student-management-gamma-drab.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
